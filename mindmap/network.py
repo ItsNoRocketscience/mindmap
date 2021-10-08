553a58807f41b56c17668f405f19ea71768fe488
ItsNoRocketscience <leonard.maisch@googlemail.com>
2021-10-08 16:14:04 +0200

    Added basic functionality Save / Load added SetupWindow added Autosize of shape added Highlight added

--- mindmap/network.py
+++ mindmap/network.py
@@ -1,34 +1,158 @@
-class Network():
+import json
+
+
+class Network:
     """
     Class definition of MindMap-Network
     """
+
     def __init__(self):
         self.master = None
-        self.entries = []
+        self.elements = []
         self.connections = []
 
     def draw(self, canvas):
-        self.master.draw()
+        self.master.draw(canvas)
 
-class Element():
+    def add_master(self, pos, s, fill_color='black', **kwargs):
+        """
+        Add Master element to network
+        :param pos: Position Tuple
+        :param s: String
+        :param fill_color: Fill color, default: 'black'
+        :return: Nothing
+        """
+        self.master = Element(None, pos, s, fill_color, **kwargs)
+        self.elements.append(self.master)
+
+    def add_element(self, parent, pos, s, **kwargs):
+        """
+        Add new element to network
+        :param parent: Element object as parent
+        :param pos: Position tuple
+        :param s: String
+        :param kwargs: Other keyword arguments
+        :return: Nothing
+        """
+        self.elements.append(Element(parent, pos, s, **kwargs))
+
+    def save(self, file_path):
+        """
+        Save Network to json file
+        :param file_path:
+        :return: Nothing
+        """
+        if self.master is None:
+            return
+
+        data = {'master': self.master.to_json()}
+        with open(file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file, ensure_ascii=True, indent=4)
+
+    @classmethod
+    def from_json(cls, file_path):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        network = cls()
+        network.master = Element.from_json(data['master'])
+        network.master.add_to_list(network.elements)
+        return network
+
+
+class Element:
     """
     Class definition of MindMap-Element
     """
-    def __init__(self, master, pos, s, size, color='black'):
-        self.master = master
+
+    def __init__(self, parent, pos, s, fill_color='black',
+                 font=('Purisa', 12), text_color='white'):
+        self.parent = parent
         self.pos = pos
-        self.string = s
-        self.size = size
-        self.color = color
+        self.s = s
+        self.shape_size = ()
+        self.fill_color = fill_color
+        self.font = font
+        self.text_color = text_color
         self.children = []
+        self.shape = None
+        self.text = None
+        self.line = None
+        self.highlighted = False
+        if self.parent is not None:
+            self.parent.children.append(self)
 
     def draw(self, canvas):
         """
         Draw Element on canvas
         """
-        canvas.create_oval(pos[0]-self.size, pos[1]-self.size,
-                           pos[0]+self.size, pos[1]+self.size, fill=self.color)
-
+        # Draw Text
+        if self.text is None:
+            self.text = canvas.create_text(
+                *self.pos, text=self.s,
+                fill=self.text_color, font=self.font)
+        # Draw Shape
+        if self.shape is None:
+            bounds = canvas.bbox(self.text)
+            x_size = (bounds[2] - bounds[0]) // 2
+            y_size = (bounds[3] - bounds[1]) // 2
+            if x_size / y_size > 4:
+                self.shape_size = (x_size * 1.2, y_size * 1.5)
+            else:
+                self.shape_size = (max(x_size, y_size), max(x_size, y_size))
+            self.shape = canvas.create_oval(
+                self.pos[0] - self.shape_size[0], self.pos[1] - self.shape_size[1],
+                self.pos[0] + self.shape_size[0], self.pos[1] + self.shape_size[1],
+                fill=self.fill_color, width=4, outline=self.fill_color)
+            canvas.tag_lower(self.shape)
+        # Create connection
+        if self.parent is not None and self.line is None:
+            self.line = canvas.create_line(
+                *self.parent.pos, *self.pos, fill='black')
+            canvas.tag_lower(self.line)
+        # Draw children
         for element in self.children:
-            element.draw()
-            # ToDo: Add connection
+            element.draw(canvas)
+
+    def switch_highlight(self, canvas):
+        self.highlighted = not self.highlighted
+        if self.highlighted:
+            canvas.itemconfigure(self.shape, outline='red')
+        else:
+            canvas.itemconfigure(self.shape, outline=self.fill_color)
+
+    def to_json(self):
+        """
+        Save Object to json dictionary
+        :return: data dictionary
+        """
+        data = {'pos': self.pos, 's': self.s, 'fill_color': self.fill_color,
+                'font': self.font, 'text_color': self.text_color, 'children': []}
+        for element in self.children:
+            data['children'].append(element.to_json())
+
+        return data
+
+    @classmethod
+    def from_json(cls, data, parent=None):
+        """
+        Create Element object from json dictionary
+        :param data: Data dictionary
+        :param parent: Parent object; default: None for master node
+        :return: element
+        """
+        element = cls(parent, **{key: data[key] for key in data.keys() if not key == 'children'})
+        for child in data['children']:
+            element.children.append(Element.from_json(child, element))
+
+        return element
+
+    def add_to_list(self, el_list):
+        """
+        Recursive Function to add elements and children to list
+        :param el_list: List of elements
+        :return: Nothing
+        """
+        el_list.append(self)
+        for element in self.children:
+            element.add_to_list(el_list)
--- mindmap/window.py
+++ mindmap/window.py
@@ -1,27 +1,131 @@
 import tkinter as tk
+import tkinter.filedialog as filedialog
+from network import Network
+
+colors = ['blue', 'black', 'red', 'green', 'white', 'cyan', 'yellow', 'magenta']
+fonts = ['Purisa', 'Helvetica', 'Times']
 
 
 class MainWindow(tk.Frame):
     """
     Class definition of main window with canvas to be drawn on
     """
+
     def __init__(self, title, master=None, width=800, height=500):
         tk.Frame.__init__(self, master)
         self.grid()
         self.master.title(title)
         # Set geometry of window
-        h_shift = (self.master.winfo_screenwidth() - width) // 2    # horizontal shift: align central
+        h_shift = (self.master.winfo_screenwidth() - width) // 2  # horizontal shift: align central
         v_shift = (self.master.winfo_screenheight() - height) // 2  # vertical shift: align central
         self.master.geometry(f'{width:d}x{height:d}+{h_shift:d}+{v_shift:d}')
 
         self.canvas = tk.Canvas(self, width=width, height=height, bg='white')
         self.canvas.grid(row=0, column=0)
 
+        self.network = Network()
+        self.highlighted = None
+
         self.canvas.bind('<Button-1>', self.click)
+        self.bind_all('<Control-s>', self.save)
+        self.bind_all('<Control-l>', self.load)
 
     def click(self, event):
         pos = (event.x, event.y)
-        print(pos)
+        # Check if clicked on existing element
+        click_id = event.widget.find_withtag('current')
+        if click_id:
+            shape_ids = [el.shape for el in self.network.elements]
+            text_ids = [el.text for el in self.network.elements]
+            if click_id[0] in shape_ids:
+                el = self.network.elements[shape_ids.index(click_id[0])]
+                self.highlight(el)
+                return
+            elif click_id[0] in text_ids:
+                el = self.network.elements[text_ids.index(click_id[0])]
+                self.highlight(el)
+                return
+        # If not add elements: Master first
+        else:
+            data = {}
+            new_window = tk.Toplevel(self.master)
+            if self.network.master is None:
+                SetupWindow(new_window, data, default_color='blue')
+            else:
+                SetupWindow(new_window, data, default_color='green')
+            self.master.wait_window(new_window)
+            if self.network.master is None:
+                self.network.add_master(pos, **data)
+            else:
+                if self.highlighted:
+                    parent = self.highlighted
+                else:
+                    parent = self.network.master
+                self.network.add_element(parent, pos, **data)
+        self.network.draw(self.canvas)
+
+    def highlight(self, element):
+        if self.highlighted is not None:
+            self.highlighted.switch_highlight(self.canvas)
+        self.highlighted = element
+        self.highlighted.switch_highlight(self.canvas)
+
+    def save(self, event):
+        if self.network.master is None:
+            return
+        file_path = filedialog.asksaveasfilename(defaultextension='json')
+        if not file_path:
+            return
+        self.network.save(file_path)
+        print('Saved Network to ' + file_path)
+
+    def load(self, event):
+        file_path = filedialog.askopenfilename()
+        if not file_path:
+            return
+
+        self.network = Network.from_json(file_path)
+        self.network.draw(self.canvas)
+        print('Loaded Network from ' + file_path)
+
+
+class SetupWindow(tk.Frame):
+    def __init__(self, master, data, default_color='blue'):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.data = data
+        self.master.title('')
+        self.master.resizable(width=False, height=False)
+        # # Set geometry of window
+        # h_shift = (self.master.winfo_screenwidth() - width) // 2  # horizontal shift: align central
+        # v_shift = (self.master.winfo_screenheight() - height) // 2  # vertical shift: align central
+        # self.master.geometry(f'{width:d}x{height:d}+{h_shift:d}+{v_shift:d}')
+        # Textbox for string
+        self.text_box = tk.Text(self, height=2, width=15)
+        self.text_box.grid(row=0, column=0)
+        self.text_box.focus_set()
+        # Dropdown list for color
+        tk.Label(self, text='Fill Color').grid(row=1, column=0, sticky=tk.W)
+        self.color_var = tk.StringVar()
+        self.color_var.set(default_color)
+        self.color_menu = tk.OptionMenu(self, self.color_var, *colors)
+        self.color_menu.grid(row=2, column=0)
+        # Dropdown list for font
+        tk.Label(self, text='Font').grid(row=3, column=0, sticky=tk.W)
+        self.font_var = tk.StringVar()
+        self.font_var.set(fonts[0])
+        self.font_menu = tk.OptionMenu(self, self.font_var, *fonts)
+        self.font_menu.grid(row=4, column=0)
+        # OK button
+        tk.Button(self, text='OK', command=self.return_values).grid(row=5, column=0, sticky='EW')
+        self.bind_all('<Return>', self.return_values)
+
+    def return_values(self, *args):
+        self.data['s'] = self.text_box.get('1.0', tk.END).rstrip()
+        self.data['fill_color'] = self.color_var.get()
+        self.data['font'] = (self.font_var.get(), 12)
+        self.master.destroy()
+
 
 if __name__ == '__main__':
     app = MainWindow(title='Testing the MainWindow-Class')
